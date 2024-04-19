b1852de0376c31fdfe7454a2ebf53006617e9949
facemonster <167509700+facemonster@users.noreply.github.com>
2024-04-19 14:42:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -64,8 +64,7 @@ const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
 // Avatar
-
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
+const jeffTexture = new THREE.TextureLoader().load('jeff.97cdd8fe.png');
 
 const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
 
